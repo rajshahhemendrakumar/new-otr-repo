b9d60f32ebb2f4a2da553473b02592ef1a523056
khush thakkar <k.thakkar2002@gmail.com>
2024-03-09 23:34:47 -0800

    addbus_admin and search bus work completation error not call api  read null value

--- backend/insert_mysql.py
+++ backend/insert_mysql.py
@@ -11,7 +11,8 @@ CORS(app, support_credentials=True)
 @app.route('/')
 def hello():
     return "Welcome to the page!"
-
+#this is another page api which
+#demo api only check ....
 @app.route('/select_frist', methods=["POST"])
 def select():
     request_data = request.get_json()
@@ -25,9 +26,9 @@ def select():
         # Assuming the first column is 'id' and the second column is 'name'
         result.append({"id": row[0], "name": row[1]})  # Adjust column indexes accordingly
 
-    return jsonify({"data": result})
+    return jsonify({"status":500,"msg": "fail",f"data":[data]})
 
-#user data store
+#user data store  reg.html
 @app.route('/insert-data', methods=['POST'])
 def insert_data():
     request_data = request.get_json()
@@ -42,8 +43,8 @@ def insert_data():
     #todo make return statement universal for every api 
     #formamt=> {"status":200,"msg":"your msg","data":[]}
     #status=>success=200,error=>500
-    return jsonify({"status": "success"})
-# payment 
+    return jsonify({"status":200,"msg": "success"})
+# payment.html
 @app.route('/payment', methods=['POST'])
 def pay():
     request_data = request.get_json()
@@ -55,9 +56,9 @@ def pay():
 
     cur.execute("INSERT INTO payment(Card_number,Card_holdername,Expiration_Date,CCV,amount) VALUES (%s, %s,%s,%s,%s)",(cardnum,cardhold,expire,ccv,amount))
     mydb.commit()
+    return jsonify({"status":200,"msg": "success"})
 
-    return jsonify({"status": "success"})
-# admin login
+# admin page login.html 
 @app.route('/login', methods=['POST'])
 def log():
     request_data = request.get_json()
@@ -71,15 +72,32 @@ def log():
     else:
          return jsonify({"status":500,"msg": "fail",f"data":[data]})
 
+# admin bus login add_bus_admin.html
+@app.route('/add_bus', methods=['POST'])
+def addbus():
+    request_data = request.get_json()
+    source= request_data['sourcek']
+    des= request_data['destinationk']
+    bus= request_data['busk']
+    location= request_data['locationk']
 
-    # result = []
-    # for row in data:
-    #     # Assuming the first column is 'id' and the second column is 'name'
-    #     result.append({"id": row[0], "name": row[1]})  # Adjust column indexes accordingly
-    
-    # print(result)
-    # return jsonify({"data": result})
+    cur.execute("INSERT INTO booking_bus_admin(Source,Destination,Bus_Number,Location) VALUES (%s, %s,%s,%s)",(source,des,bus,location))
 
+    mydb.commit()
+    return jsonify({"status":200,"msg": "success"})
+# search  bus threw normal user
+@app.route('/search_bus', methods=['POST'])
+def search():
+    request_data = request.get_json()
+    sor= request_data['sourcek']
+    to= request_data['tok']
+
+    cur.execute("select * from admin_table where UserName=%s and Password= %s",(sor,to))
+    data = cur.fetchall()
+    if(len(data)>0):
+         return jsonify({"status":200,"msg": "success",f"data":[data]})
+    else:
+         return jsonify({"status":500,"msg": "fail",f"data":[data]})
 
 if __name__ == "__main__":
     app.run(debug=True)
